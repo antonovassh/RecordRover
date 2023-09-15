06d7dbb5a2c2eda00a2d05a8929688587e529e48
Oleksandra Antonova <antonova.ssh@gmail.com>
2023-09-15 11:41:05 +0200

    Updated IndexPage

---
+++ WebAplShop/Controlers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VinylRecords.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using VinylRecords.Data.Interfaces;
+using VinylRecords.ViewModels;
+
+namespace VinylRecords.Controlers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAllPlates _storekeeper;
+        private readonly IPlateCategorizer _plateCategorizer;
+        public HomeController(IAllPlates storekeeper, IPlateCategorizer plateCategorizer)
+        {
+            _storekeeper = storekeeper;
+            _plateCategorizer = plateCategorizer;
+        }
+        public IActionResult Index()
+        {
+            PlatesListViewModel obj = new PlatesListViewModel();
+            obj.getAllPlates = _storekeeper.Plates;
+            obj.currCategory = "Plates";
+
+            return View(obj);
+        }
+    }
+}
